e09ef563d28ef65a070479615e08ab6558f94bb7
mokes <mokes@LAPTOP-CB45H386>
2024-06-08 16:32:29 +0530

    I have added a class in my name

---
+++ shoppers-stack/src/test/java/demo/Priya.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import shoppersstack.baseclass.BaseClass;
+
+public class Priya {
+	public static void main(String[] args)
+	{
+	
+	System.out.println("This is Mohanapriya");
+	}
+}
